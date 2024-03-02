aa51de13ee325a7f7e45dbfc726077c060787a94
curious <vishal238128@gmail.com>
2024-03-02 13:22:58 +0530

    url change

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,29 +1,30 @@
-import mongoose, {Mongoose} from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
+const MONGODB_URL = process.env.MONGODB_URL;
 
-const MONGODB_URL= process.env.MONGODB_URL;
-
-interface MongooseConnection{
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose;
 
-if(!cached){
-    cached = (global as any).mongoose = { conn: null, promise: null}
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn;
-    if(!MONGODB_URL) throw new Error('MongoDb url not defined')
+  if (cached.conn) return cached.conn;
+  if (!MONGODB_URL) throw new Error("MongoDb url not defined");
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-        dbName: 'pictureai',
-        bufferCommands: false,
-    })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "pictureai",
+      bufferCommands: false,
+    });
 
-    cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
 
-    return cached.conn;
-}
+  return cached.conn;
+};
